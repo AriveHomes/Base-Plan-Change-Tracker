5bf56544b33f6f0b6c13e8cae2fa833a1a63f9ac
Arive Homes <brendan@arivehomes.com>
2026-08-17 11:38:43 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,14 @@
 // Base Plan Change Tracker configuration
-// Paste your WORKING Apps Script /exec URL into all three script URL fields below.
-// Use the exact URL that worked when you tested ?action=list&callback=test.
+// Working Apps Script Web App URL
+
 window.ARIVE_CONFIG = {
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbY78fLErjt3xbrOR6pwH8kGI3CQzzkNvNYw8LeTMnxnkmnlLtTBBKQ4eeXPSQ-6yPWUP/exec',
-  GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbY78fLErjt3xbrOR6pwH8kGI3CQzzkNvNYw8LeTMnxnkmnlLtTBBKQ4eeXPSQ-6yPWUP/exec',
-  WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbY78fLErjt3xbrOR6pwH8kGI3CQzzkNvNYw8LeTMnxnkmnlLtTBBKQ4eeXPSQ-6yPWUP/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxIskfEAa281fNz-6CuPPjVbVgRMGWA4HDzu1jiIHVbpCxqSzLe2_A-8vNQSi6JayAD/exec',
+  GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxIskfEAa281fNz-6CuPPjVbVgRMGWA4HDzu1jiIHVbpCxqSzLe2_A-8vNQSi6JayAD/exec',
+  WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxIskfEAa281fNz-6CuPPjVbVgRMGWA4HDzu1jiIHVbpCxqSzLe2_A-8vNQSi6JayAD/exec',
+
   GOOGLE_SHEET_URL: 'https://docs.google.com/spreadsheets/d/1nZZrP-5gtEjAUbKmVSPSiYmQIRrUXaXQ6jucodH1gic/edit',
   SHEET_URL: 'https://docs.google.com/spreadsheets/d/1nZZrP-5gtEjAUbKmVSPSiYmQIRrUXaXQ6jucodH1gic/edit',
+
   COMPANY_NAME: 'ARIVE HOMES',
   DASHBOARD_TITLE: 'Base Plan Change Tracker'
 };
