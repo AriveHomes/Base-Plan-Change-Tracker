4c52e13edd62e8c1989020e9e3e2f9d56258f196
Arive Homes <brendan@arivehomes.com>
2026-08-17 13:16:40 -0600

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@
 // Working Apps Script Web App URL
 
 window.ARIVE_CONFIG = {
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxIskfEAa281fNz-6CuPPjVbVgRMGWA4HDzu1jiIHVbpCxqSzLe2_A-8vNQSi6JayAD/exec',
-  GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxIskfEAa281fNz-6CuPPjVbVgRMGWA4HDzu1jiIHVbpCxqSzLe2_A-8vNQSi6JayAD/exec',
-  WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxIskfEAa281fNz-6CuPPjVbVgRMGWA4HDzu1jiIHVbpCxqSzLe2_A-8vNQSi6JayAD/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyYb2sDv6kOn-w9k2BVbz7j0t-xxAIwb6Br8TbnRjGra4SMUPpVMUVAyNL0HBL8m0g/exec',
+  GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyYb2sDv6kOn-w9k2BVbz7j0t-xxAIwb6Br8TbnRjGra4SMUPpVMUVAyNL0HBL8m0g/exec',
+  WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbyYb2sDv6kOn-w9k2BVbz7j0t-xxAIwb6Br8TbnRjGra4SMUPpVMUVAyNL0HBL8m0g/exec',
 
   GOOGLE_SHEET_URL: 'https://docs.google.com/spreadsheets/d/1nZZrP-5gtEjAUbKmVSPSiYmQIRrUXaXQ6jucodH1gic/edit',
   SHEET_URL: 'https://docs.google.com/spreadsheets/d/1nZZrP-5gtEjAUbKmVSPSiYmQIRrUXaXQ6jucodH1gic/edit',
